36160886ace9c1d3bfa2991faf6905e116cf121f
Darren Chang <darren987469@gmail.com>
2018-12-08 11:38:39 +0800

    Add 403 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError do |_error|
+    render file: 'public/403.html'
+  end
 end
