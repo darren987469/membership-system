e09ae15c2c7b2bdbdc1706725917ade20b1b0f84
Darren Chang <darren987469@gmail.com>
2018-12-08 21:40:47 +0800

    Add operations of product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,11 +6,38 @@ module Admin
       @products = Product.with_attached_images.page(1).per(10)
     end
 
+    def show
+      @product = Product.find(params[:id])
+    end
+
+    def create
+      @product = Product.create(product_params)
+      redirect_to admin_product_path(@product)
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    end
+
+    def edit
+      @product = Product.find(params[:id])
+    end
+
     private
 
     def authorize_action
       policy_action = "#{action_name}?"
       authorize Product, policy_action, policy_class: Admin::ProductPolicy
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :price, images: [])
     end
   end
 end
--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe Admin::ProductsController, type: :request do
   let(:user) { create(:user) }
 
+  before { sign_in user }
+
   describe 'GET /admin/products' do
     before do
       create_list(:product, 3)
-      sign_in user
       expect_any_instance_of(Admin::ProductPolicy).to receive(:index?).and_return true
     end
 
@@ -18,6 +19,113 @@ describe Admin::ProductsController, type: :request do
       products = Product.with_attached_images.page(1).per(10)
       product_names = products.map(&:name)
       expect(response.body).to include(*product_names)
+    end
+  end
+
+  describe 'GET /admin/products/:id' do
+    let!(:product) { create(:product) }
+
+    before { expect_any_instance_of(Admin::ProductPolicy).to receive(:show?).and_return true }
+
+    subject { get admin_product_path(product) }
+
+    it 'renders successfully' do
+      subject
+      expect(response).to have_http_status :success
+      expect(response.body).to include product.name
+    end
+  end
+
+  describe 'POST /admin/products' do
+    let(:image) { fixture_file_upload('spec/fixtures/thumbs-up.png') }
+    let(:params) do
+      {
+        product: { name: 'name', price: 10, images: [image] }
+      }
+    end
+    before { expect_any_instance_of(Admin::ProductPolicy).to receive(:create?).and_return true }
+
+    subject { post admin_products_path, params: params }
+
+    it 'creates product' do
+      expect { subject }.to change { Product.count }.by(1)
+
+      product = Product.last
+      expected_attributes = params[:product].except(:images)
+      expect(product).to have_attributes(expected_attributes)
+
+      expected_image_names = [image.original_filename]
+      image_names = product.images.map { |record| record.filename.to_s }
+      expect(image_names).to match_array expected_image_names
+    end
+
+    it { is_expected.to redirect_to admin_product_path(Product.last) }
+  end
+
+  describe 'GET /admin/products/new' do
+    before { expect_any_instance_of(Admin::ProductPolicy).to receive(:new?).and_return true }
+
+    subject { get new_admin_product_path }
+
+    it 'renders successfully' do
+      subject
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe 'PATCH /admin/products/:id/edit' do
+    let!(:product) { create(:product) }
+    let(:new_price) { 111 }
+    let(:image) { fixture_file_upload('spec/fixtures/thumbs-up.png') }
+    let(:params) do
+      { product: { name: 'new_name', price: new_price, images: [] } }
+    end
+
+    before { expect_any_instance_of(Admin::ProductPolicy).to receive(:update?).and_return true }
+
+    subject { patch admin_product_path(product), params: params }
+
+    context 'update name and price' do
+      let(:params) do
+        { product: { name: 'new_name', price: new_price } }
+      end
+
+      it 'updates product attributes' do
+        expect(product).not_to have_attributes(params[:product])
+
+        subject
+
+        expect(product.reload).to have_attributes(params[:product])
+      end
+    end
+
+    context 'update image' do
+      let(:params) do
+        { product: { images: [image] } }
+      end
+
+      it 'updates product images' do
+        expect(product.images.count).to eq 0
+
+        subject
+
+        expect(product.images.count).to eq 1
+        expect(product.images.first.filename.to_s).to eq image.original_filename
+      end
+    end
+  end
+
+  describe 'GET /admin/products/:id/edit' do
+    let!(:product) { create(:product) }
+
+    before { expect_any_instance_of(Admin::ProductPolicy).to receive(:edit?).and_return true }
+
+    subject { get edit_admin_product_path(product) }
+
+    it 'renders successfully' do
+      subject
+      expect(response).to have_http_status :success
+      expect(response.body).to include product.name
     end
   end
 end
