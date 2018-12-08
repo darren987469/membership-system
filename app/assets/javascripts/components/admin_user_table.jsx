cec088a9ef78a9ed2713347906d5e880cf367056
Darren Chang <darren987469@gmail.com>
2018-12-08 16:41:52 +0800

    Add admin users page UI

---
+++ app/assets/javascripts/components/admin_user_table.jsx
@@ -0,0 +1,41 @@
+function AdminUserTable(props) {
+  const { users, currentUser, promoteToAdminRequest } = props
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Email</th>
+          <th>Membership</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          users.map(user =>
+            <UserTableRow
+              key={ user.id }
+              user= { user }
+              currentUser={ currentUser }
+              promoteToAdminRequest={ promoteToAdminRequest }
+            />
+          )
+        }
+      </tbody>
+    </table>
+  )
+}
+
+function UserTableRow(props) {
+  const { user, currentUser, promoteToAdminRequest } = props
+  let button = null
+  if(user.id != currentUser.id && (user.role === 'normal' || user.role === 'premium')){
+    button = <button onClick={() => promoteToAdminRequest(user.id)}>Promote to Admin</button>
+  }
+  return(
+    <tr>
+      <td>{user.email}</td>
+      <td>{user.role}</td>
+      <td>{button}</td>
+    </tr>
+  )
+}
