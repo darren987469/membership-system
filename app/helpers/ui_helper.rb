7782f56e50b3b0be479945057f67bda38ebc10ea
Darren Chang <darren987469@gmail.com>
2018-12-09 18:11:34 +0800

    Refactor: extract default button class

---
+++ app/helpers/ui_helper.rb
@@ -0,0 +1,5 @@
+module UiHelper
+  def default_button_class
+    'w3-button w3-section w3-teal w3-ripple'
+  end
+end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,8 +2,7 @@ module UserHelper
   def upgrade_to_premium_button
     return nil unless current_user.normal?
 
-    button_class = 'w3-button w3-section w3-teal w3-ripple'
-    button_to('Upgrade to premium', upgrade_to_premium_path, method: :patch, class: button_class)
+    button_to('Upgrade to premium', upgrade_to_premium_path, method: :patch, class: default_button_class)
   end
 
   def link_to_admin
