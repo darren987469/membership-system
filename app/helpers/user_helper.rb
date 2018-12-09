15ce5a2ab8a1eef81d8eeaef7fd193d6c3ac1a5c
Darren Chang <darren987469@gmail.com>
2018-12-09 17:56:19 +0800

    Add go to admin link

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -5,4 +5,10 @@ module UserHelper
     button_class = 'w3-button w3-section w3-teal w3-ripple'
     button_to('Upgrade to premium', upgrade_to_premium_path, method: :patch, class: button_class)
   end
+
+  def link_to_admin
+    return nil unless current_user.admin? || current_user.owner?
+
+    link_to('Go to Admin', admin_products_path, class: 'w3-bar-item w3-button')
+  end
 end
