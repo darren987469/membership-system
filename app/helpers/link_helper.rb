506f9f002129fc9a381fdb97964724fa3e580983
Darren Chang <darren.chang@tarokosoftware.com>
2018-12-10 15:02:11 +0800

    Add link helper link_to_admin_or_website

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,7 +1,18 @@
 module LinkHelper
   def link_to_admin
     return nil unless current_user.admin? || current_user.owner?
+    return nil if controller.class < Admin::BaseController
 
     link_to('Go to Admin', admin_products_path, class: 'w3-bar-item w3-button')
+  end
+
+  def link_to_website
+    return nil if controller.class == ProductsController
+
+    link_to('Go to Website', products_path, class: 'w3-bar-item w3-button')
+  end
+
+  def link_to_admin_or_website
+    controller.class < Admin::BaseController ? link_to_website : link_to_admin
   end
 end
--- spec/helpers/link_helper_spec.rb
+++ spec/helpers/link_helper_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-describe LinkHelper do
+describe LinkHelper, type: :helper do
   include described_class
+
+  let(:current_user) { create(:user, role: :owner) }
 
   describe '#link_to_admin' do
     subject { link_to_admin }
+
+    context 'already in admin pages' do
+      before { expect(self).to receive(:controller) { Admin::ProductsController.new } }
+      it { is_expected.to be_nil }
+    end
 
     context 'admin user' do
       let(:current_user) { create(:user, role: :admin) }
@@ -24,6 +31,35 @@ describe LinkHelper do
     context 'premium user' do
       let(:current_user) { create(:user, role: :normal) }
       it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#link_to_website' do
+    subject { link_to_website }
+
+    context 'already in /products page' do
+      before { expect(self).to receive(:controller) { ProductsController.new } }
+      it { is_expected.to be_nil }
+    end
+
+    context 'not in /products page' do
+      it { is_expected.to include('Go to Website', 'href="/products"') }
+    end
+  end
+
+  describe '#link_to_admin_or_website' do
+    subject { link_to_admin_or_website }
+
+    context 'in admin pages' do
+      before { expect(self).to receive(:controller).twice { Admin::UsersController.new } }
+
+      it { is_expected.to include('Go to Website', 'href="/products"') }
+    end
+
+    context 'not in admin page' do
+      before { expect(self).to receive(:controller).twice { ProductsController.new } }
+
+      it { is_expected.to include('Go to Admin', 'href="/admin/products"') }
     end
   end
 end
